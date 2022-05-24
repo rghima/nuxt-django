138a2738e474853fe415848d0a0bded9d2bbce94
Debian <mehdi.achach@hotmail.fr>
2022-05-24 04:25:50 -0500

    seconde commit

--- backend/django/mysite/settings.py
+++ backend/django/mysite/settings.py
@@ -128,8 +128,8 @@ REST_FRAMEWORK = {
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
-TIME_ZONE = 'Asia/Tokyo'
+LANGUAGE_CODE = 'fr'
+TIME_ZONE = 'Europe/Paris'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
